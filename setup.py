ef53fdfd02c8a4378705d3a63396f89b2f6edc0a
Bicky23 <bijitdeka23@gmail.com>
2020-05-17 18:52:01 +0530

    made installable

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from setuptools import setup
 setup(name='stocks',
       packages=['stocks'],
       version='0.0.1dev1',
+      entry_points={
+            'console_scripts': ['stocks-cmd=stocks.cmd:main']
+      }
       )
